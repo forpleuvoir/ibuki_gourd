e72d01d77d073c6803d59e1e8f6e0446edbd79d3
forpleuvoir <695801070@qq.com>
2022-12-27 17:50:43 +0800

    服务端语言施工

---
+++ src/main/kotlin/com/forpleuvoir/ibukigourd/config/ClientModConfigManager.kt
@@ -0,0 +1,11 @@
+package com.forpleuvoir.ibukigourd.config
+
+import com.forpleuvoir.nebula.config.ConfigManager
+import com.forpleuvoir.nebula.config.ConfigSerializable
+import com.forpleuvoir.nebula.serialization.base.SerializeElement
+
+interface ClientModConfigManager :ConfigManager {
+
+
+
+}
---
+++ src/main/kotlin/com/forpleuvoir/ibukigourd/mod/config/IbukiGourdServerConfig.kt
@@ -0,0 +1,10 @@
+package com.forpleuvoir.ibukigourd.mod.config
+
+import com.forpleuvoir.nebula.config.item.impl.ConfigString
+
+object IbukiGourdServerConfig {
+
+	var SERVER_LANGUAGE by ConfigString("serve_language", "zh_cn")
+
+
+}
