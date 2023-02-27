a0a998a9ca4603fa4e84b0dbccb4d4d6459b8e2c
forpleuvoir <695801070@qq.com>
2023-02-27 22:26:21 +0800

    1.19.3

--- src/main/kotlin/com/forpleuvoir/ibukigourd/event/IbukiGourdEventManager.kt
+++ src/main/kotlin/com/forpleuvoir/ibukigourd/event/IbukiGourdEventManager.kt
@@ -14,6 +14,7 @@ object IbukiGourdEventManager : EventManager() {
 		}
 	}
 
+	@Suppress("unchecked_cast")
 	override fun subscribe() {
 		scanPackage { it.java.isAnnotationPresent(EventSubscriber::class.java) }
 			.map { it.java }
