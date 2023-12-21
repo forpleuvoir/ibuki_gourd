337d10bd7cec89f7099ed28588fe6d30a45b7694
forpleuvoir <695801070@qq.com>
2023-12-21 18:08:25 +0800

    DraggableList 实现中

--- src/main/kotlin/moe/forpleuvoir/ibukigourd/gui/base/layout/DraggableList.kt
+++ src/main/kotlin/moe/forpleuvoir/ibukigourd/gui/base/layout/DraggableList.kt
@@ -10,14 +10,17 @@ import moe.forpleuvoir.ibukigourd.render.RenderContext
 import moe.forpleuvoir.ibukigourd.render.base.Arrangement
 import moe.forpleuvoir.ibukigourd.render.base.math.Vector3
 import moe.forpleuvoir.ibukigourd.render.base.vertex.vertex
+import moe.forpleuvoir.ibukigourd.render.graphics.rectangle.rect
+import moe.forpleuvoir.ibukigourd.render.helper.renderRect
 import moe.forpleuvoir.ibukigourd.render.helper.renderRoundRect
 import moe.forpleuvoir.ibukigourd.render.helper.useBlend
 import moe.forpleuvoir.ibukigourd.util.NextAction
 import moe.forpleuvoir.nebula.common.color.Color
-import java.util.*
+import moe.forpleuvoir.nebula.common.color.Colors
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
+import kotlin.math.abs
 
 /**
  * 应该为下一级元素可被拖动
@@ -55,13 +58,12 @@ class DraggableList(
 
         override fun onExit() {
             if (targetIndex != -1 && targetIndex != subElements.indexOf(draggingElement)) {
-                Collections.swap(subElements, targetIndex, subElements.indexOf(draggingElement))
+                subElements.removeAt(subElements.indexOf(draggingElement))
+                subElements.add(targetIndex, draggingElement!!)
                 elementSwap(targetIndex, subElements.indexOf(draggingElement))
-                arrange()
-            } else {
-                draggingElement?.transform?.translate(-draggingOffset)
-                draggingElement = null
             }
+            arrange()
+            draggingElement = null
             targetIndex = -1
         }
 
@@ -73,10 +75,37 @@ class DraggableList(
 
         override fun onMouseDragging(mouseX: Float, mouseY: Float, button: Mouse, deltaX: Float, deltaY: Float) {
             draggingElement?.transform?.translate(arrangement.switch(vertex(0f, deltaY, 0f), vertex(deltaX, 0f, 0f)))
-            arrangeElements.filter { it != draggingElement }.find {
-                TODO("鼠标位置附近最近的两个元素,如果在元素内则选择当前元素的前后")
-            }?.let {
-                elementIndexOf(it)
+            // TODO("鼠标位置附近最近的两个元素,如果在元素内则选择当前元素的前后")
+            arrangeElements.filter { it != draggingElement }.minByOrNull { element ->
+                arrangement.switch({
+                    if (element.transform.worldTop < mouseY) {
+                        abs(element.transform.worldBottom - mouseY)
+                    } else {
+                        abs(element.transform.worldTop - mouseY)
+                    }
+                }, {
+                    if (element.transform.worldLeft < mouseX) {
+                        abs(element.transform.worldRight - mouseX)
+                    } else {
+                        abs(element.transform.worldLeft - mouseX)
+                    }
+                })
+            }?.let { element ->
+                val index = elementIndexOf(element)
+                targetIndex = arrangement.switch({
+                    if (element.transform.worldTop < mouseY) {
+                        index + 1
+                    } else {
+                        index
+                    }
+                }, {
+                    if (element.transform.worldLeft < mouseX) {
+                        index + 1
+                    } else {
+                        index
+                    }
+                })
+                if (index == elementIndexOf(draggingElement!!)) targetIndex = -1
             }
         }
 
@@ -107,15 +136,7 @@ class DraggableList(
     private var pressed: Boolean = false
 
     var draggingElement: Element? = null
-        private set(value) {
-            if (value == null) {
-                draggingCounter = 0
-                field?.transform?.translate(-draggingOffset)
-            } else {
-                draggingElementOldPosition = value.transform.position
-            }
-            field = value
-        }
+        private set
 
     private var draggingElementOldPosition: Vector3<out Float>? = null
 
@@ -157,7 +178,7 @@ class DraggableList(
         if (button == Mouse.LEFT && pressed) {
             pressed = false
         }
-        swapElements(mouseX, mouseY)
+//        swapElements(mouseX, mouseY)
         return super.onMouseRelease(mouseX, mouseY, button)
     }
 
@@ -181,7 +202,11 @@ class DraggableList(
         }
         fixedElements.forEach { it.render(renderContext) }
         scrollerBar.render(renderContext)
-        draggingElement?.let { renderDraggingElement(renderContext, it) }
+        draggingElement?.let {
+            renderDraggingElement(renderContext, it)
+            renderTargetIndex(renderContext, targetIndex)
+        }
+
         renderOverlay.invoke(renderContext)
     }
 
@@ -192,6 +217,16 @@ class DraggableList(
                 renderRoundRect(renderContext.matrixStack, it.transform.asWorldRect, Color(0x7F8CECFF), 2)
             }
         }
+    }
+
+    fun renderTargetIndex(renderContext: RenderContext, targetIndex: Int) {
+        if (targetIndex == -1 || targetIndex == elementIndexOf(draggingElement!!)) return
+        val element = subElements[targetIndex]
+        arrangement.switch({
+            renderRect(renderContext.matrixStack, rect(element.transform.worldPosition + vertex(0f, -1f, 0f), element.transform.width, 1f), Colors.BLACK)
+        }, {
+            renderRect(renderContext.matrixStack, rect(element.transform.worldPosition + vertex(-1f, 0f, 0f), 1f, element.transform.height), Colors.BLACK)
+        })
     }
 
 }
