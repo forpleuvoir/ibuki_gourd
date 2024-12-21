19bbf0750f94055cd3f77e21b47c510543293949
forpleuvoir <695801070@qq.com>
2024-12-21 19:02:56 +0800

    新增命令构建DSL及测试命令实现
    - 添加 `CommandBuildDSL.kt` 文件，提供基于DSL的命令构建工具，支持注册命令、参数设置、子命令层级、权限校验等功能。

---
+++ src/devClientTest/kotlin/moe/forpleuvoir/ibukigourd/test/TestCommand.kt
@@ -0,0 +1,58 @@
+package moe.forpleuvoir.ibukigourd.test
+
+import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.arguments.StringArgumentType
+import moe.forpleuvoir.ibukigourd.command.dsl.registerCommand
+import moe.forpleuvoir.ibukigourd.event.events.client.ClientCommandRegisterEvent
+import moe.forpleuvoir.nebula.event.EventSubscriber
+import moe.forpleuvoir.nebula.event.Subscriber
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
+
+@EventSubscriber
+object TestCommand {
+
+    @Subscriber
+    fun register(event: ClientCommandRegisterEvent) {
+        println("注册指令...")
+        event.dispatcher.testCommand()
+    }
+
+    fun CommandDispatcher<FabricClientCommandSource>.testCommand() = registerCommand("igtest") {
+        requires {
+            this.hasPermissionLevel(2)
+        }
+        execute {
+            println("直接执行了 test")
+        }
+        "sub1" {
+            execute {
+                println("执行了子节点 sub1")
+            }
+            "又是一层" {
+                execute {
+                    println("执行了子节点 sub1 又是一层")
+                }
+                "又又是一层" {
+                    execute {
+                        println("执行了子节点 sub1 又是一层 又又是一层")
+                    }
+                    argument("arg1", StringArgumentType.string()) {
+                        suggests("arg1", "arg2")
+                        execute {
+                            val arg = StringArgumentType.getString(this, "arg1")
+                            println("执行了子节点 sub1 又是一层 又又是一层 参数节点:$arg")
+                        }
+                    }
+                }
+            }
+        }
+        argument("arg1", StringArgumentType.string()) {
+            suggests("arg1", "arg2")
+            execute {
+                val arg = StringArgumentType.getString(this, "arg1")
+                println("执行了参数节点:$arg")
+            }
+        }
+
+    }
+}
